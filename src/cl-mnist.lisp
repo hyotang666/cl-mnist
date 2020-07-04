6644541a956566c845dfa52ccd6fe5383bfc8349
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-07-04 18:52:55 +0900

    Extend: LOAD-IMAGES to support &key FORCE.

--- src/cl-mnist.lisp
+++ src/cl-mnist.lisp
@@ -68,32 +68,52 @@
           (dotimes (x size array) (setf (aref array x (aref result x)) 1)))
         result)))
 
-(defun load-images (pathname &key normalize flatten)
-  (with-open-file (in pathname :element-type '(unsigned-byte 8))
-    (fast-io:with-fast-input (buff nil in)
-      (fast-io:read32-be buff) ; Discard magic number.
-      (let* ((items (fast-io:read32-be buff))
-             (row (fast-io:read32-be buff))
-             (col (fast-io:read32-be buff))
-             (size (* row col items))
-             (result (make-array size :element-type '(unsigned-byte 8))))
-        (assert (= size (fast-io:fast-read-sequence result buff)))
-        (if normalize
-            (make-array
-              (cons items
-                    (if flatten
-                        (list (* row col))
-                        (list row col)))
-              :element-type *read-default-float-format*
-              :displaced-to (map `(array ,*read-default-float-format* (*))
-                                 (lambda (x) (/ x 255.0)) result))
-            (make-array
-              (cons items
-                    (if flatten
-                        (list (* row col))
-                        (list row col)))
-              :displaced-to result
-              :element-type '(unsigned-byte 8)))))))
+(defun load-images (pathname &key normalize flatten force)
+  (let (result size items row col)
+    ;; Loading...
+    (if (or force
+            (not (probe-file pathname))
+            (not (probe-file (make-pathname :type "lisp" :defaults pathname))))
+        (with-open-file (in pathname :element-type '(unsigned-byte 8))
+          (fast-io:with-fast-input (buff nil in)
+            (fast-io:read32-be buff) ; Discard magic number.
+            (setf items (fast-io:read32-be buff)
+                  row (fast-io:read32-be buff)
+                  col (fast-io:read32-be buff)
+                  size (* row col items)
+                  result (make-array size :element-type '(unsigned-byte 8)))
+            (assert (= size (fast-io:fast-read-sequence result buff)))
+            (with-standard-io-syntax
+             (with-open-file (out (make-pathname :type "lisp"
+                                                 :defaults pathname))
+               (print items out)
+               (print row out)
+               (print col out)
+               (print result out)))))
+        (with-open-file (in (make-pathname :type "lisp" :defaults pathname))
+          (with-standard-io-syntax
+           (setf items (read in)
+                 row (read in)
+                 col (read in)
+                 size (* row col)
+                 result (read in)))))
+    ;; Modifying...
+    (if normalize
+        (make-array
+          (cons items
+                (if flatten
+                    (list (* row col))
+                    (list row col)))
+          :element-type *read-default-float-format*
+          :displaced-to (map `(array ,*read-default-float-format* (*))
+                             (lambda (x) (/ x 255.0)) result))
+        (make-array
+          (cons items
+                (if flatten
+                    (list (* row col))
+                    (list row col)))
+          :displaced-to result
+          :element-type '(unsigned-byte 8)))))
 
 (defun load-mnist (&key force normalize flatten one-hot-label)
   "Return plist as :train-labels :test-labels :train-images :test-images."
